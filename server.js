e2a153e08db577fde213e0b02de211c21f49a1af
josh <jbweb89@yahoo.com>
2020-04-08 13:14:18 -0400

    add database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const profile = require("./controllers/profile");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-clear-00459",
-    user: "josh",
-    password: "",
-    database: "smartbrain",
+    host: process.env.DATABASE_URL,
+    ssl: "true",
   },
 });
 
