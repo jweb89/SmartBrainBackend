94b0ac2cd139f298d5863ee9264f564bff31df0a
josh <jbweb89@yahoo.com>
2020-04-08 15:55:09 -0400

    1

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const image = require("./controllers/image");
 const profile = require("./controllers/profile");
-const { db } = require("pg");
-db = knex({
+const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
