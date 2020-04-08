eefac9b485516f87a89e91091647d12f88c77ec4
josh <jbweb89@yahoo.com>
2020-04-08 16:39:15 -0400

    debug

--- controllers/register.js
+++ controllers/register.js
@@ -18,7 +18,7 @@ const handleRegister = (req, res, db, bcrypt) => {
           .insert({
             id: 1,
             email: loginEmail[0],
-            name: name,
+            name: name[0],
             joined: new Date(),
             entries: 0,
           })
