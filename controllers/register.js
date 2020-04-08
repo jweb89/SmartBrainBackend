963bf79dc3ae52f9bb75c2340b90d9e53491c333
josh <jbweb89@yahoo.com>
2020-04-08 16:54:49 -0400

    a

--- controllers/register.js
+++ controllers/register.js
@@ -13,6 +13,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .into("login")
       .returning("email")
       .then((loginEmail) => {
+        console.log(name, email, new Date());
         return trx("users")
           .returning("*")
           .insert({
